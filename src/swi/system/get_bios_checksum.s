205dad6a24ce5c7bbc6900a6e385856abf35d3fb
Emi Huang <coolbho3k@users.noreply.github.com>
2026-09-16 02:42:32 -0700

    Restore caller's interrupt masks (#10)
    * Disable IRQ and FIQ during GetBiosChecksum
    
    * Restore caller's interrupt masks
    
    * Update get_bios_checksum.s
    
    * Update bios.sha256

--- src/swi/system/get_bios_checksum.s
+++ src/swi/system/get_bios_checksum.s
@@ -9,18 +9,17 @@
 swi_GetBiosChecksum:
     # Disable IRQ and FIQ
     mrs r1, cpsr
-    orr r1, r1, #(IRQ_DISABLE | FIQ_DISABLE)
-    msr cpsr_c, r1
+    orr r3, r1, #(IRQ_DISABLE | FIQ_DISABLE)
+    msr cpsr_c, r3
     ldr r0, =0xbaae187f
     .set GBC_COUNT, 10237
     ldr r3, =GBC_COUNT
 .gbc_loop:
     subs r3, r3, #1
     bne  .gbc_loop
+    mov  r3, r3, lsl r3
     mov  r3, #0x4000
     # Re-enable IRQ and FIQ
-    mrs r1, cpsr
-    bic r1, r1, #(IRQ_DISABLE | FIQ_DISABLE)
     msr cpsr_c, r1
     mov r1, #1
     bx  lr
